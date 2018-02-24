077f961960393a498d4bcbbeb17836a7a6de7285
sirsha.banerjee <sirsha.banerjee@indianic.com>
2018-02-24 18:52:29 +0530

    Initial commit

---
+++ app/src/main/java/com/banerjee/sirsha/mediaplayerdemo/activity/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.banerjee.sirsha.mediaplayerdemo.activity;
+
+import android.os.Bundle;
+
+import com.banerjee.sirsha.mediaplayerdemo.R;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected int defineLayoutResource() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initializeComponents() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
